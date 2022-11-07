04f6ebd3cb6114622896f14dc830a4b6755c5ba4
Orchid <maks67topcheg@yandex.ru>
2022-11-07 13:56:10 +0300

    create game brain-prime

---
+++ src/games/brain-prime.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import { cons } from '@hexlet/pairs';
+
+function prime(n) {
+  if (n < 2) {
+    return false;
+  } if (n === 2) {
+    return true;
+  }
+
+  let i = 2;
+  const limit = Math.sqrt(n);
+  while (i <= limit) {
+    if (n % i === 0) {
+      return false;
+    }
+    i += 1;
+  }
+
+  return true;
+}
+
+export default () => {
+  const number = Math.round(Math.random() * 100);
+  let correctAns = '';
+  if (prime(number)) {
+    correctAns = 'yes';
+  } else {
+    correctAns = 'no';
+  }
+
+  return cons(number, correctAns);
+};
