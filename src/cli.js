891b98df5d8dc05aacd0be7ea1013c5b4bcacd41
OrchidClouse <maks67topcheg@yadnex.ru>
2023-02-23 18:48:57 +0300

    fix?

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
-#!/usr/bin/env node
-
+/* eslint-disable linebreak-style */
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line import/no-extraneous-dependencies
 import readlineSync from 'readline-sync';
 
 export default function greet() {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
   return console.log(`Hello, ${username}!`);
-};
+}
